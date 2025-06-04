e82e3dd4637c52297edaa42b01a5288a35fab6a9
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-04 16:58:49 +0200

    Update HTML title for clarity and implement function to count vowels in a string

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -3,11 +3,28 @@
 const word = 'javascript';
 
 
-// Dichiara la funzione qui.
 
+// Dichiara la funzione qui.
+function numberOfVocals(word) {
+    const vocalsOfString = [];
+    const vocals = ["a", "e", "i", "o", "u"];
+    for (let i = 0; i < word.length; i++) {
+        const thisLetter = word[i];
+        if (vocals.includes(thisLetter.toLowerCase())) {
+            vocalsOfString.push(thisLetter);
+        }
+    }
+    return vocalsOfString.length, (vocalsOfString);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const vocalsOfJavascript = numberOfVocals(word);
+console.log(vocalsOfJavascript);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
+
+// if (thisLetter === thisVocal) {
+//  const thisVocal = vocals[i];
