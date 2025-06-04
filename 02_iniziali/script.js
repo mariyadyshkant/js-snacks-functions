b21e9198fb3273221c4663f012a1765cda5cf35e
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-04 14:57:00 +0200

    Implement function to extract initials from an array of names

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function nameInitials(array) {
+    const initials = [];
+    for (let i = 0; i < array.length; i++) {
+        const name = array[i];
+        const initial = name[0];
+        initials.push(initial);
+    }
+    return initials;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const initials = nameInitials(names);
+console.log(initials);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
