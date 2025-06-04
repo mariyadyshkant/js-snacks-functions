b55f90cf5b51f504dc2bae007b17b7fb117812b6
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-04 15:43:08 +0200

    Refactor wordsWithSpecificInitial function to improve clarity and functionality

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,9 +4,27 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+/**
+ * La funzione restituisce un array contenente i nomi di un array precedentemente creato, selezionati in base alla letter iniziale.
+ * @param {Array} array - array contenente i nomi da selezionare
+ * @param {string} letter - lettere iniziale delle parole da inserire nell'array della funzione
+ * @returns {Array} - array contenente le parole con iniziano con la lettera scelta come argomento
+ */
+function wordsWithSpecificInitial(array, letter) {
+    const wordsArray = [];
+    for (let i = 0; i < array.length; i++) {
+        const thisWord = array[i];
+        const wordInitial = thisWord[0];
+        if (wordInitial.toUpperCase() === letter.toUpperCase()) {
+            wordsArray.push(thisWord);
+        }
+    }
+    return wordsArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const namesWithInitialA = wordsWithSpecificInitial(names, "A");
+console.log(namesWithInitialA);
 
 
 
